96c6ffb6251d1ce248dc12985c0e83d07f7d5ad4
Evgeniy Sokolovskiy <cokol-rc@yandex.ru>
2022-11-26 21:58:17 +0300

    build: setup build scripts & vite config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,15 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+
 dotenv.config()
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: Number(process.env.CLIENT_PORT) || 3000,
-  },
-  define: {
-    __SERVER_PORT__: process.env.SERVER_PORT,
-  },
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  return {
+    server: {
+      port: Number(process.env.CLIENT_PORT) || 3000
+    },
+    define: {
+      __SERVER_PORT__: process.env.SERVER_PORT
+    },
+    plugins: [react()],
+    build: {
+      manifest: false,
+      minify: mode === 'development' ? false : 'terser',
+      sourcemap: command === 'serve' ? 'inline' : false,
+
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name][extname]',
+          chunkFileNames: '[name].[hash].js',
+          entryFileNames: '[name].js'
+        }
+      }
+    }
+  }
 })
