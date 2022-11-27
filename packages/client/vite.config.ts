1386246a095ab8666e4ed3f7e8ad3c385a50abd0
Evgeniy Sokolovskiy <cokol-rc@yandex.ru>
2022-11-27 14:03:49 +0300

    feat: add alias for ./src

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+import * as path from 'path'
 
 dotenv.config()
 
@@ -12,6 +13,11 @@ export default defineConfig(({ command, mode }) => {
     },
     define: {
       __SERVER_PORT__: process.env.SERVER_PORT
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     },
     plugins: [react()],
     build: {
