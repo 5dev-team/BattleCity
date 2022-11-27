ff0ba3b44252786144db095731f09527e33d3eb2
Evgeniy Sokolovskiy <cokol-rc@yandex.ru>
2022-11-27 18:03:27 +0300

    style: lint autofix

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import SignIn from './pages/sign-in'
 
-foo = 11;
-const foo = 12;
-foo = 12412;
-
 const App: React.FC = () => {
   return (
     <Router>
