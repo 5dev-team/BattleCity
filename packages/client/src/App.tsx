bb863d1bf5a403aa23cbb89d13d680be37174932
Aleksandr Savostin <asavostin1@gmail.com>
2022-11-29 23:53:47 +0500

    feat: Реализованы error404, error500, forum pages

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import LeaderBoard from '@/pages/leaderboard'
-import SignIn from '@/pages/sign-in'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LeaderBoard from '@/pages/leaderboard';
+import SignIn from '@/pages/sign-in';
 import Game from '@/pages/game';
+import Error404 from '@/pages/error404';
+import Error500 from '@/pages/error500';
+import Forum from '@/pages/forum';
+
+export enum RoutePaths {
+  SIGNIN = '/sign-in',
+  LEADERBOARD = '/leaderboard',
+  GAME = '/game',
+  ERROR404 = '/error404',
+  ERROR500 = '/error500',
+  FORUM = '/forum',
+}
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/leaderboard' element={<LeaderBoard />} />
-        <Route path='/game' element={<Game />} />
+        <Route path={RoutePaths.SIGNIN} element={<SignIn />} />
+        <Route path={RoutePaths.LEADERBOARD} element={<LeaderBoard />} />
+        <Route path={RoutePaths.GAME} element={<Game />} />
+        <Route path={RoutePaths.ERROR404} element={<Error404 />} />
+        <Route path={RoutePaths.ERROR500} element={<Error500 />} />
+        <Route path={RoutePaths.FORUM} element={<Forum />} />
       </Routes>
     </Router>
   )
 }
 
-export default App
+export default App;
---
+++ packages/client/src/pages/forum/forum.tsx
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import styles from './forum.module.scss';
+import NesButton from '@/components/UI/nes-button';
+import NesInput from '@/components/UI/nes-input';
+
+export type ForumThemesType = {
+    title: string,
+    author: string,
+    answers: number,
+    lastAnswer: string,
+};
+
+const Forum: React.FC = () => {
+    const getDate = (): string => {
+        return new Date().toLocaleString('ru', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            weekday: 'long',
+        });
+    };
+    
+    const [templateThem, setTemplateThem] = useState({
+        title: '',
+        author: 'Admin',
+        answers: 0,
+        lastAnswer: getDate(),
+    });
+
+    const [themes, setThem] = useState([
+        {
+            title: 'WorldOfTank',
+            author: 'Admin',
+            answers: 27,
+            lastAnswer: '12.12.2022',
+        },
+        {
+            title: 'TankOfWorld',
+            author: 'Domin',
+            answers: 9,
+            lastAnswer: '12.12.2022',
+        },
+        {
+            title: 'WorldOfWorld',
+            author: 'Mindon',
+            answers: 11,
+            lastAnswer: '12.12.2022',
+        }
+    ]);
+
+    const titles = ['Тема', 'Автор', 'Ответы', 'Последний']
+        .map((el, id) => <div key={id} className={`${styles['forum-header-item']}`}>{el}</div>);
+
+    const addThemes = (themes: Array<ForumThemesType>) => {
+        return themes
+            .map((theme, id) => <div key={id} className={`${styles['forum-theme']}`}>{Object.values(theme)
+                .map((el, id) => <div key={id} className={`${styles['forum-theme-item']}`}>{el}</div>)}</div>);
+    };
+
+    const addThem = (themes: Array<ForumThemesType>, templateThem: ForumThemesType): void => {
+        templateThem.title && setThem([...themes, templateThem]);
+    };
+
+    return (
+        <div className={`${styles['forum']}`}>
+            <div className={`${styles['forum-add-them']}`}>
+                <NesButton 
+                    variant='warning'
+                    onClick={() => addThem(themes, templateThem)}
+                >
+                    Добавить
+                </NesButton>
+                <NesInput
+                    inline
+                    label='Название темы'
+                    onChange={evt => setTemplateThem({...templateThem, title: evt.target.value})}
+                />
+            </div>
+            <div className={`${styles['forum-header']}`}>{titles}</div>
+            <div className={`${styles['forum-themes']}`}>{addThemes(themes)}</div>
+        </div>
+    );
+}
+
+export default Forum;
