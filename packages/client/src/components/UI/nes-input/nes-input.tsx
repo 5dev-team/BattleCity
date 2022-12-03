6655620f16b15532648877924c5fcdf0b99627b1
Evgeniy Sokolovskiy <cokol-rc@yandex.ru>
2022-12-03 03:42:19 +0300

    style: add readonly style

--- packages/client/src/components/UI/nes-input/nes-input.tsx
+++ packages/client/src/components/UI/nes-input/nes-input.tsx
@@ -6,6 +6,8 @@ interface INesInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string
   variant?: 'basic' | 'primary' | 'success' | 'warning' | 'error' | 'dark'
   fullWidth?: boolean
+  labelHidden?: boolean
+  plainText?: boolean
 }
 
 const NesInput: React.FC<INesInputProps> = ({
@@ -13,15 +15,23 @@ const NesInput: React.FC<INesInputProps> = ({
   label,
   variant = 'basic',
   fullWidth,
+  labelHidden,
+  plainText,
   ...props
 }) => {
   const id = useId()
   return (
     <div className={`nes-field ${inline ? 'is-inline' : ''}`}>
-      <label htmlFor={id}>{label}</label>
+      <label
+        className={`${labelHidden ? styles['visually-hidden'] : ''}`}
+        htmlFor={id}>
+        {label}
+      </label>
       <input
         id={id}
-        className={`nes-btn is-${variant} ${styles['nes-btn']}`}
+        className={`is-${variant} ${styles['nes-btn']} ${
+          plainText ? styles['plaintext'] : 'nes-btn'
+        }`}
         style={fullWidth ? { width: '100%' } : {}}
         {...props}
       />
