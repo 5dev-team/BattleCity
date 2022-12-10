721f81816d830fdd743f1e5f9f2183775d3f436d
Иван <thechertan@yandex.ru>
2022-12-10 20:34:19 +0300

    fix: rename constants image and add svg for arrow

--- packages/client/src/pages/screen-game-over/screen-game-over.tsx
+++ packages/client/src/pages/screen-game-over/screen-game-over.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import styles from './screen-game-over.module.scss'
-import T0 from '@/assets/tanks/1T.png'
-import T1 from '@/assets/tanks/2T.png'
-import T2 from '@/assets/tanks/3T.png'
-import T3 from '@/assets/tanks/4T.png'
+import T1 from '@/assets/tanks/T1.png'
+import T2 from '@/assets/tanks/T2.png'
+import T3 from '@/assets/tanks/T3.png'
+import T4 from '@/assets/tanks/T4.png'
+import arrow from '@/assets/arrow.svg'
+
 import GameButton from '@/components/UI/game-button'
 import GameMenu from '@/components/UI/game-menu/game-menu'
 
@@ -13,7 +15,7 @@ const ScreenGameOver: React.FC = () => {
 
   type Stats = Partial<{ P1: number, P2: number, key: number, T: string }>
 
-  interface inputProps {
+  interface IinputProps {
     nextGame: boolean
     hiScore: number,
     stage: number,
@@ -21,16 +23,16 @@ const ScreenGameOver: React.FC = () => {
     playersStats: Stats[]
   }
 
-  const props: inputProps = {
-    nextGame: false,
+  const props: IinputProps = {
+    nextGame: true,
     hiScore: 20000,
     stage: 1,
     players: 2,
     playersStats: [
-      { P1: 0, P2: 1, key: 0, T: T0 },
-      { P1: 1, P2: 1, key: 1, T: T1 },
-      { P1: 1, P2: 1, key: 2, T: T2 },
-      { P1: 20, P2: 4, key: 3, T: T3 }
+      { P1: 0, P2: 1, key: 0, T: T1 },
+      { P1: 1, P2: 1, key: 1, T: T2 },
+      { P1: 1, P2: 1, key: 2, T: T3 },
+      { P1: 20, P2: 4, key: 3, T: T4 }
     ]
   }
 
@@ -52,7 +54,7 @@ const ScreenGameOver: React.FC = () => {
     }, { P1: 0, P2: 0 })
 
 
-  const template = props.playersStats.map((value, index: number) => {
+  const template = props.playersStats.map((value: Stats, index: number) => {
     return (
       <li className={`${styles['players-stats__item']} ${props.players === 1 && styles['players-stats__item_one']}`}
           key={value.key}>
@@ -63,12 +65,13 @@ const ScreenGameOver: React.FC = () => {
         <div
           className={`${styles['players-stats__type-tank']} ${props.players === 1 && styles['players-stats__type-tank_one']}`}>
           <p className={styles['players-stats__text']}>{value.P1 ? value.P1 : 0}</p>
-          <p className={styles['players-stats__arrow']}>🡄</p>
+          <img src={arrow}  alt='arrow' className={styles['players-stats__arrow']}/>
           <img src={value.T} alt='tank' className={styles['players-stats__image-tank']} />
           {props.players === 2 &&
             <>
-              <p className={`${styles['players-stats__arrow']} ${styles['players-stats__arrow_p2']}`}>🡄</p>
-              <p className={`${styles['players-stats__text']} ${styles['players-stats__text_p2']}`}>{value.P2 ? value.P2: 0}</p>
+              <img src={arrow}  alt='arrow' className={`${styles['players-stats__arrow']} ${styles['players-stats__arrow_p2']}`}/>
+              <p
+                className={`${styles['players-stats__text']} ${styles['players-stats__text_p2']}`}>{value.P2 ? value.P2 : 0}</p>
             </>
           }
         </div>
@@ -147,15 +150,15 @@ const ScreenGameOver: React.FC = () => {
             <GameButton onClick={() => navigate('/game')}>
               REPEAT
             </GameButton>
-            }
-            <GameButton onClick={() => navigate('/leaderboard')}>
-          LEADERBOARD
-        </GameButton>
-        <GameButton onClick={() => navigate('/')}>EXIT</GameButton>
-      </GameMenu>
-    </div>
-</section>
-)
+          }
+          <GameButton onClick={() => navigate('/leaderboard')}>
+            LEADERBOARD
+          </GameButton>
+          <GameButton onClick={() => navigate('/')}>EXIT</GameButton>
+        </GameMenu>
+      </div>
+    </section>
+  )
 }
 
 export default ScreenGameOver
