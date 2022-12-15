5296048a08f11c30ddc0831c2ac8600601bf2854
Underwindow <genya@podoinikov.ru>
2022-12-16 01:06:37 +0500

    feat(store): added profile slice

---
+++ packages/client/src/store/slices/profile/index.ts
@@ -0,0 +1,74 @@
+import api from '@/api'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { fetchUser } from '@/store/slices/auth'
+import { IFetchProfileData } from '@/store/slices/profile/profile.models'
+
+export const fetchProfile = createAsyncThunk(
+  'users/fetchProfile',
+  (data: IFetchProfileData, { dispatch }) => {
+    const promises: Promise<unknown>[] = []
+
+    if (data.profileData) {
+      promises.push(api.users.changeProfile(data.profileData))
+    }
+    if (data.passwordData) {
+      promises.push(api.users.changePassword(data.passwordData))
+    }
+    if (data.avatarData) {
+      promises.push(api.users.changeAvatar(data.avatarData))
+    }
+
+    return Promise.allSettled(promises).then(results => {
+      dispatch(fetchUser())
+
+      const rejectReasons = results
+        .filter(({ status }) => status === 'rejected')
+        .map(result => (result as PromiseRejectedResult).reason)
+        .join(', ')
+
+      if (rejectReasons) {
+        dispatch(profileSlice.actions.serError(rejectReasons))
+      }
+
+      return results
+    })
+  }
+)
+
+interface IInitialState {
+  isLoading: boolean
+  fetchError: string
+}
+
+const initialState: IInitialState = {
+  isLoading: false,
+  fetchError: '',
+}
+
+export const profileSlice = createSlice({
+  name: 'profile',
+  initialState,
+  reducers: {
+    serError: (state, action) => {
+      state.fetchError = action.payload
+    },
+    clearError: state => {
+      state.fetchError = ''
+    },
+  },
+  extraReducers: builder => {
+    builder.addCase(fetchProfile.pending, state => {
+      state.isLoading = true
+    })
+    builder.addCase(fetchProfile.fulfilled, state => {
+      state.isLoading = false
+    })
+    builder.addCase(fetchProfile.rejected, (state, { error }) => {
+      state.isLoading = false
+
+      if (error.message) {
+        state.fetchError = error.message
+      }
+    })
+  },
+})
