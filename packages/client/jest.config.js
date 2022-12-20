448669b31c2ad27653044880b4e4e486c818c18c
Иван <thechertan@yandex.ru>
2022-12-20 06:04:30 +0300

    fix: test

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -10,7 +10,7 @@ export default {
   },
   // css, scss imports mock
   moduleNameMapper: {
-    '\\.(css|scss|png|svg|)$': 'identity-obj-proxy',
+    '\\.(css|scss|png|svg|jpg)$': 'identity-obj-proxy',
     "^@/(.*)$": "<rootDir>/src/$1",
   }
 }
