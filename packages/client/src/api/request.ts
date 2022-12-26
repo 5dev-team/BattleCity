fbefca0c80a79968bd8cca57ecdd0acde366607a
Evgeniy Sokolovskiy <cokol-rc@yandex.ru>
2022-12-27 00:32:46 +0300

    fix: return request and 401 handler

--- packages/client/src/api/request.ts
+++ packages/client/src/api/request.ts
@@ -1,5 +1,7 @@
 import type { AxiosRequestConfig } from 'axios'
 import axios from 'axios'
+import { RoutePaths } from '@/App'
+import history from '@/utils/history'
 
 axios.interceptors.response.use(
   response => {
@@ -7,6 +9,11 @@ axios.interceptors.response.use(
     return response.data
   },
   error => {
+    if (error.response.status === 401) {
+      if (RoutePaths.SIGNIN.valueOf() !== window.location.pathname && RoutePaths.SIGNUP.valueOf() !== window.location.pathname) {
+        history.push(RoutePaths.SIGNIN)
+      }
+    }
     return Promise.reject(error.response.data.reason || 'Server error')
   }
 )
