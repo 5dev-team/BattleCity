6035f33d3839b9b2b2a67a51a154e6b26f6f575f
exact-buckwheat <84693047+thechertan@users.noreply.github.com>
2023-01-15 00:06:39 +0300

    New task 76 craeta hoc and hook protect route (#38)
    * feat: add Protect-Route and hook useAuth
    
    * fix: 404 for everyone. Also a bit refactor
    
    * fix: 404 for everyone. Also a bit refactor
    
    * fix: sign-in only for auth
    
    * refactor: normal import
    
    * fix: sing-in auth
    
    * fix: typescript in profile page
    
    * fix: refactor after the sync team
    
    * fix: remove console log onp page game
    
    * fix: return request and 401 handler
    
    * refactor: test Redux createAsyncThunk
    
    * fix: return axios response
    
    * refactor: delete bugs
    
    * refactor: last commit
    
    * refactor: some last commit
    
    Co-authored-by: Evgeniy Sokolovskiy <cokol-rc@yandex.ru>
    Co-authored-by: sokol-rc <83005607+sokol-rc@users.noreply.github.com>

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -9,10 +9,13 @@ import Error500 from '@/pages/error500'
 import Forum from '@/pages/forum'
 import SignUp from '@/pages/sign-up/sign-up'
 import Profile from '@/pages/profile'
+import ProtectRoute from '@/components/protect-route'
 import { useAppDispatch, useAppSelector } from '@/hooks/redux'
 import { fetchUser } from '@/store/slices/auth'
 import history from '@/utils/history'
 import Offline from '@/pages/offline'
+import ProtectRouteForSignIn from '@/components/protect-route-for-sign-in'
+import { interceptor } from '@/api/request'
 
 export enum RoutePaths {
   SIGNIN = '/sign-in',
@@ -27,28 +30,36 @@ export enum RoutePaths {
 
 const App: React.FC = () => {
   const dispatch = useAppDispatch()
-  
+
   const user = useAppSelector((state) => state.auth.user)
   useEffect(() => {
     if (!user) {
       dispatch(fetchUser())
+      interceptor(dispatch)
     }
-  }, [])
-  
+  }, [user])
+
+
   return (
     <Router history={history}>
       <Routes>
-        <Route path='/offline' element={<Offline />} />
-        <Route path={RoutePaths.SIGNIN} element={<SignIn />} />
-        <Route path={RoutePaths.SIGNUP} element={<SignUp />} />
-        <Route element={<FullScreen/>}>
-          <Route path={RoutePaths.GAME} element={<Game />} />
+        <Route element={<ProtectRouteForSignIn redirectTo={RoutePaths.GAME} />}>
+          <Route path={RoutePaths.SIGNIN} element={<SignIn />} />
+          <Route path={RoutePaths.SIGNUP} element={<SignUp />} />
         </Route>
-        <Route path={RoutePaths.LEADERBOARD} element={<Leaderboard />} />
+        <Route element={<ProtectRoute redirectTo={RoutePaths.SIGNIN} />}>
+          <Route element={<Leaderboard />} path={RoutePaths.LEADERBOARD} />
+          <Route element={<FullScreen/>}>
+            <Route path={RoutePaths.GAME} element={<Game />} />
+          </Route>
+          <Route path={RoutePaths.FORUM} element={<Forum />} />
+          <Route element={<Forum />} path={RoutePaths.FORUM} />
+          <Route element={<Profile />} path={RoutePaths.PROFILE} />
+          <Route path={RoutePaths.PROFILE} element={<Profile />} />
+        </Route>
+        <Route path='/offline' element={<Offline />} />
         <Route path={RoutePaths.ERROR404} element={<Error404 />} />
         <Route path={RoutePaths.ERROR500} element={<Error500 />} />
-        <Route path={RoutePaths.FORUM} element={<Forum />} />
-        <Route path={RoutePaths.PROFILE} element={<Profile />} />
         <Route path='*' element={<Navigate to={RoutePaths.ERROR404} replace />} />
       </Routes>
     </Router>
