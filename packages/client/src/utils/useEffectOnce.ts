2654f0a29ba2d6c9b1da7aebdb1f1da7dc369f76
Underwindow <genya@podoinikov.ru>
2023-01-17 18:07:13 +0500

    feat(utils): useEffectOnce hook

---
+++ packages/client/src/utils/useEffectOnce.ts
@@ -0,0 +1,13 @@
+import { useRef, useEffect } from 'react';
+
+export default function useEffectOnce(fn: () => unknown) {
+  const ref = useRef(false)
+  useEffect(() => {
+    if (ref.current) {
+      fn()
+    }
+    return () => {
+      ref.current = true
+    }
+  }, [])
+}
