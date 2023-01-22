2eeb7cd0e76ce3978ebb912cf59398a6d31ab294
Evgeny Podoynikov <35759068+Underwindow@users.noreply.github.com>
2023-01-22 16:49:48 +0500

    refactor: refactor after review

--- packages/client/src/utils/useEffectOnce.ts
+++ packages/client/src/utils/useEffectOnce.ts
@@ -1,6 +1,6 @@
 import { useRef, useEffect } from 'react'
 
-export default function useEffectOnce(fn: () => unknown) {
+export default function useEffectOnce(fn: () => void) {
   const ref = useRef(false)
   useEffect(() => {
     if (ref.current) {
