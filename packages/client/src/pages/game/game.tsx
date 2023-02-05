dd157db7b40db2b436e7ff355f22bf34b5321a5b
Иван <thechertan@yandex.ru>
2023-02-06 01:42:04 +0300

    refactor: remake GameOver page

--- packages/client/src/pages/game/game.tsx
+++ packages/client/src/pages/game/game.tsx
@@ -12,10 +12,6 @@ import View from '@/game/core/view'
 import SpriteAtlas from '@/game/sprites/sprite_1.png'
 import Sprite from '@/game/core/sprite'
 import { levels as Levels } from '@/game/helpers/levels'
-import T1 from '@/assets/tanks/T1.png'
-import T2 from '@/assets/tanks/T2.png'
-import T3 from '@/assets/tanks/T3.png'
-import T4 from '@/assets/tanks/T4.png'
 import styles from './game.module.scss'
 
 enum GameView {
@@ -55,37 +51,32 @@ const Game: React.FC = () => {
 
     }
   },[])
-  const gameOverProps = {
-    nextGame: true,
-    hiScore: 20000,
+
+  const initialState = {
+    nextGame: false,
     stage: 1,
-    players: { count: 2, totalPts: { P1: 1000, P2: 1000 } },
-    playersStats: [
-      {
-        P1: { count: 1, total: 100 },
-        P2: { count: 1, total: 100 },
-        key: 0,
-        T: T1,
+    playersCount: 2,
+    bestScore: 0,
+    player1: {
+      user: 3213213,
+      scores: {
+        1: { count: 2, points: 200 },
+        2: { count: 3, points: 600 },
+        3: { count: 4, points: 1200 },
+        4: { count: 5, points: 2000 }
       },
-      {
-        P1: { count: 1, total: 200 },
-        P2: { count: 1, total: 200 },
-        key: 1,
-        T: T2,
+      total: 7000
+    },
+    player2: {
+      user: 3213123,
+      scores: {
+        1: { count: 2, points: 200 },
+        2: { count: 3, points: 600 },
+        3: { count: 4, points: 1200 },
+        4: { count: 5, points: 2000 }
       },
-      {
-        P1: { count: 1, total: 300 },
-        P2: { count: 1, total: 400 },
-        key: 2,
-        T: T3,
-      },
-      {
-        P1: { count: 1, total: 400 },
-        P2: { count: 1, total: 400 },
-        key: 3,
-        T: T4,
-      },
-    ],
+      total: 7000
+    }
   }
 
   const initGame = (gameMode: GameMode) => {
@@ -165,12 +156,12 @@ const Game: React.FC = () => {
           ></canvas>
         )}
         {gameView === GameView.GameOver && (
-          <GameOver {...gameOverProps}>
+          <GameOver {...initialState}>
             <GameMenu
               selectItemId={0}
               className={styles['game-over-page-buttons']}
             >
-              {gameOverProps.nextGame ? (
+              {initialState.nextGame ? (
                 <GameButton onClick={() => console.log('load next level')}>
                   NEXT LEVEL
                 </GameButton>
