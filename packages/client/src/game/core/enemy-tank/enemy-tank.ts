af08239dd9e62036dd73028fb9adc7e61ff22163
Evgeniy Sokolovskiy <cokol-rc@yandex.ru>
2023-02-12 12:50:21 +0300

    revert: fix merge

--- packages/client/src/game/core/enemy-tank/enemy-tank.ts
+++ packages/client/src/game/core/enemy-tank/enemy-tank.ts
@@ -11,14 +11,14 @@ import { Direction, IUpdatable, UpdateState, Vec2 } from '@/game/core/types'
 
 export default class EnemyTank extends Tank implements IUpdatable {
   public type: number
-
+  
   constructor(type: number) {
     super({ pos: Vec2.zero, sprites: ENEMY_TANK_SPRITES })
     this.direction = Direction.Down
     this.type = type
     this.name = 'enemy-tank'
   }
-
+  
   private invertDirection() {
     switch (this.direction) {
       case Direction.Down:
@@ -35,7 +35,7 @@ export default class EnemyTank extends Tank implements IUpdatable {
         break
     }
   }
-
+  
   public rotateClockwise() {
     if (this.direction !== Direction.Left) {
       this.direction = this.direction + 1
@@ -43,7 +43,7 @@ export default class EnemyTank extends Tank implements IUpdatable {
       this.direction = Direction.Up
     }
   }
-
+  
   public rotateAntiClockwise() {
     if (this.direction !== Direction.Up) {
       this.direction = this.direction - 1
@@ -51,7 +51,7 @@ export default class EnemyTank extends Tank implements IUpdatable {
       this.direction = Direction.Left
     }
   }
-
+  
   private changeDirectionWhenTileReach() {
     if (Math.round(Math.random()) === 0) {
       this.changeDirection()
@@ -62,41 +62,41 @@ export default class EnemyTank extends Tank implements IUpdatable {
       this.rotateAntiClockwise()
     }
   }
-
+  
   private changeDirection() {
     // const periodDuration = this.respawn / 8
   }
-
+  
   public setPosition(positionIndex: number) {
     this.pos = new Vec2(
       ENEMY_TANK_START_POSITIONS[positionIndex][0],
       ENEMY_TANK_START_POSITIONS[positionIndex][1]
     )
   }
-
+  
   public update({ world, frameDelta }: Omit<UpdateState, 'input'>): void {
     const direction = this.direction
     const axis = getAxisForDirection(direction)
     const value = getValueForDirection(direction)
-
+    
     const gameObjects = Array.from(world.gameObjects)
-
+    
     this.turn(direction, this.getTurnOffsetLimit(gameObjects))
-
+    
     const collisions = this.getCollisions(this.getColliders(gameObjects))
-
+    
     let isOutOfBounds = false
     if (collisions.length === 0) {
       const movement = this.getMovement(this.getMoveOffsetLimit(gameObjects))
       this.move(axis, value * movement)
-
+      
       if ((isOutOfBounds = world.isOutOfBounds(this)))
         this.move(axis, -value * movement)
     }
-
+    
     if (isOutOfBounds || collisions.length > 0) {
       const rand = Math.round(Math.random() * 4)
-
+      
       if (rand % 4 == 0) {
         if (this.pos.x % 8 !== 0 || this.pos.y % 8 !== 0) {
           this.invertDirection()
@@ -105,9 +105,9 @@ export default class EnemyTank extends Tank implements IUpdatable {
         }
       }
     }
-
+    
     this.animate(frameDelta)
-
+    
     if (Math.floor(Math.random() * 31) === 1) {
       this.fire()
     }
