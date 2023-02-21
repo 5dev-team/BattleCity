1842adf9ccd4bd43c7c49902e0976e3457d5dee8
Иван <thechertan@yandex.ru>
2023-02-21 22:18:46 +0300

    fix: remove console.log in protect route

--- packages/client/src/components/protect-route-for-sign-in/ProtectRouteForSignIn.tsx
+++ packages/client/src/components/protect-route-for-sign-in/ProtectRouteForSignIn.tsx
@@ -7,7 +7,6 @@ interface IProps {
 
 const ProtectRouteForSignIn = ({redirectTo = '/'}: IProps) => {
   const isAuth: boolean | null = useAuth()
-  console.log(isAuth)
   if (isAuth !== null && isAuth) {
     return <Navigate to={redirectTo} replace />
   }
